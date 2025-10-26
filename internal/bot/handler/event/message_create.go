4f759f6fbb42136290bba5bb6f524c4ea88210a5
Lewis Cook <lcook@FreeBSD.org>
2025-10-26 16:42:43 +0000

    antispam: handle attachment spam

--- internal/bot/handler/event/message_create.go
+++ internal/bot/handler/event/message_create.go
@@ -9,12 +9,13 @@ import (
 	"crypto/sha512"
 	"encoding/hex"
 	"slices"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func (h *Handler) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID || m.Member == nil || m.Author.Bot || m.Content == "" {
+	if m.Author.ID == s.State.User.ID || m.Member == nil || m.Author.Bot {
 		return
 	}
 
@@ -28,8 +29,21 @@ func (h *Handler) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate
 		}
 	}
 
+	var content strings.Builder
+	for _, attachment := range m.Attachments {
+		content.WriteString(attachment.Filename)
+	}
+
+	if m.Content != "" {
+		content.WriteString(m.Content)
+	}
+
+	if content.String() == "" {
+		return
+	}
+
 	sha := sha512.New()
-	sha.Write([]byte(m.Content))
+	sha.Write([]byte(content.String()))
 
 	hash := hex.EncodeToString(sha.Sum(nil))
 
@@ -39,17 +53,3 @@ func (h *Handler) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate
 		h.AntiSpam(s, m, hash)
 	}
 }
-
-/*
-	panic: runtime error: invalid memory address or nil pointer dereference
-	[signal SIGSEGV: segmentation violation code=0x1 addr=0x68 pc=0x7dc447]
-
-	goroutine 6316 [running]:
-	github.com/lcook/pulsar/internal/bot/handler/event.(*Handler).MessageCreate(0x870126400, 0x87011ae08, 0x8702065b8)
-	        /home/lcook/dev/pulsar/internal/bot/handler/event/message_create.go:26 +0x107
-	github.com/bwmarrin/discordgo.messageCreateEventHandler.Handle(0x87003e008?, 0x8706b47d0?, {0x940980?, 0x8702065b8?})
-	        /home/lcook/go/pkg/mod/github.com/bwmarrin/discordgo@v0.29.0/eventhandlers.go:926 +0x33
-	created by github.com/bwmarrin/discordgo.(*Session).handle in goroutine 5935
-	        /home/lcook/go/pkg/mod/github.com/bwmarrin/discordgo@v0.29.0/event.go:171 +0x12c
-
-*/
--- internal/bot/handler/event/message_delete.go
+++ internal/bot/handler/event/message_delete.go
@@ -13,7 +13,7 @@ import (
 )
 
 func (h *Handler) MessageDelete(s *discordgo.Session, m *discordgo.MessageDelete) {
-	if m.BeforeDelete == nil || m.Member == nil {
+	if m.BeforeDelete == nil {
 		return
 	}
 
