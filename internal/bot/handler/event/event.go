140c13fa4b0fc8c331a90740ecf75be14c988842
Lewis Cook <lcook@FreeBSD.org>
2025-11-17 11:22:40 +0000

    antispam: refactor spam processing to separate function

--- internal/bot/handler/event/event.go
+++ internal/bot/handler/event/event.go
@@ -4,6 +4,12 @@
 package event
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+
 	"github.com/lcook/pulsar/internal/antispam"
 	"github.com/lcook/pulsar/internal/cache"
 	"github.com/lcook/pulsar/internal/config"
@@ -34,4 +40,94 @@ func New(settings config.Settings, buffer uint64) *Handler {
 	h.Events = append(h.Events, h.AutoModExecution)
 
 	return h
+}
+
+func (h *Handler) ProcessSpam(
+	session *discordgo.Session,
+	message *discordgo.MessageCreate,
+	logs []*antispam.Log,
+	rule *antispam.HeuristicRule,
+) {
+	timeout := time.Now().Add(rule.Timeout)
+	session.GuildMemberTimeout(message.GuildID, message.Author.ID, &timeout)
+
+	bucket := make(map[string][]string)
+
+	for idx := range logs {
+		log := logs[idx]
+		bucket[log.Message.ChannelID] = append(
+			bucket[log.Message.ChannelID],
+			log.Message.ID,
+		)
+	}
+
+	h.Logs.ForEach(func(l *antispam.Log) {
+		for idx := range logs {
+			log := logs[idx]
+			if l.Message.ID == log.Message.ID {
+				l.MarkDeleted()
+				break
+			}
+		}
+	})
+
+	var deleted int
+
+	for channel, ids := range bucket {
+		if err := session.ChannelMessagesBulkDelete(channel, ids); err == nil {
+			deleted += len(ids)
+		}
+	}
+
+	channels := make([]string, 0, len(bucket))
+	for channel := range bucket {
+		channels = append(channels, fmt.Sprintf("<#%s>", channel))
+	}
+
+	var fields []*discordgo.MessageEmbedField
+
+	if rule.Duplicated {
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:   "Contents",
+			Value:  buildContentField(message.Content, message.Attachments),
+			Inline: true,
+		})
+	}
+
+	fields = append(fields, &discordgo.MessageEmbedField{
+		Name:   "Channel(s)",
+		Value:  strings.Join(channels, " "),
+		Inline: true,
+	})
+
+	if deleted > 1 &&
+		canViewChannel(session, message.GuildID, message.ChannelID) {
+		session.ChannelMessageSendEmbed(
+			h.Settings.LogChannel,
+			&discordgo.MessageEmbed{
+				Title: ":shield: Anti-spam alert",
+				Description: fmt.Sprintf(
+					"%d message(s) automatically removed from %d channel(s) due to suspected spam or advertising activity by <@%s>. The user has been timed out for %s. _Please exercise caution: these messages may contain malicious links, phishing attempts, or other harmful content_.",
+					deleted,
+					len(channels),
+					message.Author.ID,
+					rule.Timeout.String(),
+				),
+				Timestamp: time.Now().Format(time.RFC3339),
+				Color:     embedDeleteColor,
+				Footer: &discordgo.MessageEmbedFooter{
+					Text: fmt.Sprintf(
+						"ID: %s | HEURISTIC: %s",
+						message.Author.ID,
+						rule.ID,
+					),
+				},
+				Author: &discordgo.MessageEmbedAuthor{
+					Name:    message.Author.Username,
+					IconURL: message.Author.AvatarURL("256"),
+				},
+				Fields: fields,
+			},
+		)
+	}
 }
--- internal/bot/handler/event/message_create.go
+++ internal/bot/handler/event/message_create.go
@@ -6,10 +6,8 @@ package event
 import (
 	"crypto/sha512"
 	"encoding/hex"
-	"fmt"
 	"slices"
 	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 
@@ -60,86 +58,6 @@ func (h *Handler) MessageCreate(
 			return
 		}
 
-		timeout := time.Now().Add(rule.Timeout)
-		s.GuildMemberTimeout(m.GuildID, m.Author.ID, &timeout)
-
-		bucket := make(map[string][]string)
-
-		for idx := range logs {
-			log := logs[idx]
-			bucket[log.Message.ChannelID] = append(
-				bucket[log.Message.ChannelID],
-				log.Message.ID,
-			)
-		}
-
-		h.Logs.ForEach(func(l *antispam.Log) {
-			for idx := range logs {
-				log := logs[idx]
-				if l.Message.ID == log.Message.ID {
-					l.MarkDeleted()
-					break
-				}
-			}
-		})
-
-		var deleted int
-
-		for channel, ids := range bucket {
-			if err := s.ChannelMessagesBulkDelete(channel, ids); err == nil {
-				deleted += len(ids)
-			}
-		}
-
-		channels := make([]string, 0, len(bucket))
-		for channel := range bucket {
-			channels = append(channels, fmt.Sprintf("<#%s>", channel))
-		}
-
-		var fields []*discordgo.MessageEmbedField
-
-		if rule.Duplicated {
-			fields = append(fields, &discordgo.MessageEmbedField{
-				Name:   "Contents",
-				Value:  buildContentField(m.Content, m.Attachments),
-				Inline: true,
-			})
-		}
-
-		fields = append(fields, &discordgo.MessageEmbedField{
-			Name:   "Channel(s)",
-			Value:  strings.Join(channels, " "),
-			Inline: true,
-		})
-
-		if deleted > 1 && canViewChannel(s, m.GuildID, m.ChannelID) {
-			s.ChannelMessageSendEmbed(
-				h.Settings.LogChannel,
-				&discordgo.MessageEmbed{
-					Title: ":shield: Anti-spam alert",
-					Description: fmt.Sprintf(
-						"%d message(s) automatically removed from %d channel(s) due to suspected spam or advertising activity by <@%s>. The user has been timed out for %s. _Please exercise caution: these messages may contain malicious links, phishing attempts, or other harmful content_.",
-						deleted,
-						len(channels),
-						m.Author.ID,
-						rule.Timeout.String(),
-					),
-					Timestamp: time.Now().Format(time.RFC3339),
-					Color:     embedDeleteColor,
-					Footer: &discordgo.MessageEmbedFooter{
-						Text: fmt.Sprintf(
-							"ID: %s | HEURISTIC: %s",
-							m.Author.ID,
-							rule.ID,
-						),
-					},
-					Author: &discordgo.MessageEmbedAuthor{
-						Name:    m.Author.Username,
-						IconURL: m.Author.AvatarURL("256"),
-					},
-					Fields: fields,
-				},
-			)
-		}
+		h.ProcessSpam(s, m, logs, rule)
 	}
 }
